9cd4287ab6a714266ebf1f63ea3af586de1efbd5
Maki <69772418+Nedoko-maki@users.noreply.github.com>
2022-04-22 15:50:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import logging
-
-import gui
-import PyQt6.QtWidgets as Widgets
 import sys
+import PyQt6.QtWidgets as Widgets
+import gui
 
 
 class MainWindow(gui.MainWindowBase):
     def __init__(self):
-        super().__init__(config_filepath="gui_config.json")
+        super().__init__(config_path="gui_config.json")
+        self.init_ui()  # I could hide these method calls in the parent class, but I think having the flexibility of
+        # initialising the object at different times is nice.
+        self.show()
         self.thread_manager = gui.ThreadManager()
 
     def do_this(self):
@@ -15,15 +16,11 @@ class MainWindow(gui.MainWindowBase):
         self.thread_manager.start_thread("square_thread")
 
     def print_ans(self):
-        self.print_log(self.thread_manager.wait_thread("square_thread"))
+        self.log_print(self.thread_manager.wait_thread("square_thread"))
 
     @staticmethod
     def print_squares(n):
-        ret = []
-        for i in range(1, n+1):
-            ret.append(i**2)
-
-        return ret
+        return [i ** 2 for i in range(1, n + 1)]
 
 
 def main():
